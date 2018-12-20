0e601f0c08a36e2b91776e722e4a8587a9a82627
davidzangardi <davidzangardi@gmail.com>
2018-12-20 16:56:13 +0800

    fetch

--- fetch.py
+++ fetch.py
@@ -1,6 +1,5 @@
-import os
-import json
 import requests
+import os
 
 # fetch conferences
 c = requests.get('https://api.collegefootballdata.com/conferences')
@@ -10,9 +9,29 @@ with open('/tmp/conferences.tmp.json', 'w') as f:
 
 os.rename('/tmp/conferences.tmp.json', '/tmp/conferences.json')
 
+conferences = c.json()
+
 # fetch teams
-for conference in json.loads(c):
+for conference in conferences:
     a = conference['abbreviation']
     t = requests.get(
         'https://api.collegefootballdata.com/teams?conference=' + a)
-    print(json.loads(t))
+
+    with open('/tmp/' + a + '.tmp.json', 'w') as g:
+        g.write(t.text)
+    os.rename('/tmp/' + a + '.tmp.json', '/tmp/' + a + '.json')
+
+    teams = t.json()
+
+# fetch rosters
+    for team in teams:
+        s = team['school']
+        s1 = s.replace(" ", "%20").replace("&", "%26")
+        s2 = s.replace(" ", "_").replace("'", " ").replace(
+            "&", "").replace("(", "").replace(")", "")
+        r = requests.get(
+            'https://api.collegefootballdata.com/roster?team=' + s1)
+
+        with open('/tmp/' + s2 + '.tmp.json', 'w') as h:
+            h.write(r.text)
+        os.rename('/tmp/' + s2 + '.tmp.json', '/tmp/' + s2 + '.json')
